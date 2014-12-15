36b81fb069bf4bc9b3351fa2e797e8480b6a3abd
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2014-12-15 10:02:42 +0100

    Upgrade to SWORD service version 1.4.1

--- puppet/modules/sword/manifests/install.pp
+++ puppet/modules/sword/manifests/install.pp
@@ -1,23 +1,25 @@
 class sword::install {
   require tomcat-server
 
-  $sword_war_url = "https://github.com/slub/sword-fedora/releases/download/v1.4.0/sword-fedora-1.4.0-slub.war"
-  $sword_home_path = "/opt/sword"
-  $sword_module_path = "/vagrant/puppet/modules/sword"
+  $sword_version     = '1.4.1'
+  $sword_war_url     = "https://github.com/slub/sword-fedora/releases/download/v${sword_version}/sword-fedora-${sword_version}.war"
+  $sword_home_path   = '/opt/sword'
+  $sword_module_path = '/vagrant/puppet/modules/sword'
 
   file { [
-    "${sword_home_path}", 
+    "${sword_home_path}",
     "${sword_home_path}/bin",
+    "${sword_home_path}/lib",
     "${sword_home_path}/log",
     "${sword_home_path}/config"
   ]:
     ensure => 'directory',
-    group => tomcat7,
-    mode => "g+rwx"
+    group  => 'tomcat7',
+    mode   => 'g+rwx'
   }->
-  exec { "download-sword":
+  exec { 'download-sword':
     command => "wget ${sword_war_url} -O sword.war",
-    cwd => "${sword_home_path}/bin",
+    cwd     => "${sword_home_path}/bin",
     creates => "${sword_home_path}/bin/sword.war"
   }->
   file { "${sword_home_path}/bin/sword.xml":
