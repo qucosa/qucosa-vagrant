5a841907d11e896d0c1e9273017b2883a81e722e
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2019-03-08 16:13:01 +0100

    Remove Puppet and its tooling

--- puppet/environments/vagrant/modules/oaiprovider/manifests/metadataformat.pp
+++
@@ -1,9 +0,0 @@
-define oaiprovider::metadataformat ($loc, $uri, $diss, $about = undef) {
-
-  concat::fragment { "proai.properties.metaformat ${name}": 
-    content => template('oaiprovider/proai.properties.metaformat.erb'),
-    target  => "${::oaiprovider::config::config_dir}/proai.properties",
-    order   => '03'
-  }
-
-}
--- puppet/environments/vagrant/site/roles/manifests/qucosa/search.pp
+++
@@ -1,3 +0,0 @@
-class roles::qucosa::search inherits roles::qucosa {
-  include profiles::qucosa::elasticsearch
-}
